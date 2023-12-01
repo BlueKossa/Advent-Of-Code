b05df27e02f391a0924e484131747a941313a390
BlueKossa <dexter@stormdal.se>
2023-12-01 14:16:12 +0100

    2023 Day 1

---
+++ 2023/day_1/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int char_to_int(char c) {
+    return c - '0';
+}
+
+int slice_to_int(char* slice) {
+    const char numbers[9][6] = {
+            "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
+    };
+    for (int i = 0; i < 9; i++) {
+        const char* number = numbers[i];
+        // Check if slice starts with number
+        if (strncmp(slice, number, strlen(number)) == 0) {
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
+char* read_file(char* path) {
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return NULL;
+    }
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* buffer = malloc(length + 1);
+    if (buffer) {
+        fread(buffer, 1, length, file);
+    }
+    fclose(file);
+    return buffer;
+}
+
+int solve(char* input, int part_two) {
+    int length = strlen(input);
+    int sum = 0;
+    int first = -1;
+    int last = -1;
+    for (int i = 0; i < length; i++) {
+        char c = input[i];
+        // Reset
+        if (c == '\n') {
+            sum += first * 10 + last;
+            first = -1;
+            last = -1;
+            continue;
+        }
+        int n = char_to_int(c);
+        if (n > 0 && n < 10) {
+            if (first == -1) {
+                first = n;
+            }
+            last = n;
+            continue;
+        }
+        if (part_two) {
+            char* slice = (char*)(input + i);
+            int number = slice_to_int(slice);
+            if (number == -1) {
+                continue;
+            }
+            if (first == -1) {
+                first = number;
+            }
+            last = number;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    char* input = read_file("input.txt");
+    printf("Part 1: %d\n", solve(input, 0));
+    printf("Part 2: %d\n", solve(input, 1));
+    return 0;
+}
